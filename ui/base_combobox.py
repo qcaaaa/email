9bca17a304488a22017fea293690e1e2110bb8da
yyp <huchong@rorke.com.cn>
2023-07-27 17:57:32 +0800

    style

--- ui/base_combobox.py
+++ ui/base_combobox.py
@@ -1,9 +1,9 @@
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QLineEdit, QComboBox
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QLineEdit, QComboBox, QListWidget, QListWidgetItem
 from typing import Tuple
 
 
-class BaseComboBox:
+class BaseComboBox(QtWidgets.QWidget):
     def __init__(self, parent, tuple_size: Tuple[int, ...] = None, lst_data: list = None,
                  file_style: str = '', is_clear: bool = True):
         """
@@ -13,19 +13,26 @@ class BaseComboBox:
         :param file_style: 加载样式
         :param is_clear: 是否清空初始值
         """
+        super().__init__(parent)
         self.parent = parent
         self.tuple_size = tuple_size
         self.lst_data = lst_data
         self.file_style = file_style
         self.is_clear = is_clear
         self.comboBox = QComboBox(self.parent)
-        self.lineEdit = QtWidgets.QLineEdit(self.parent)
-        self.listWidget = QtWidgets.QListWidget(self.parent)
+        self.lineEdit = QLineEdit(self.parent)
+        self.listWidget = QListWidget(self.parent)
+        self.listWidget.setStyleSheet(self.file_style)
+        self.listWidget.itemClicked.connect(self.updateLineEdit)
         if self.lst_data:
-            self.allItem = QtWidgets.QListWidgetItem(self.listWidget)
-            self.allItem.setText("全选")
-            self.allItem.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
-            self.allItem.setCheckState(QtCore.Qt.Unchecked)
+            self.allItem = self.set_data('全选')
+
+    def set_data(self, str_data: str):
+        item = QListWidgetItem(self.listWidget)
+        item.setText(str_data)
+        item.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
+        item.setCheckState(QtCore.Qt.Unchecked)
+        return item
 
     @property
     def box(self) -> QComboBox:
@@ -38,29 +45,29 @@ class BaseComboBox:
             self.comboBox.setStyleSheet(self.file_style)
 
         for i in self.lst_data:
-            item = QtWidgets.QListWidgetItem(self.listWidget)
-            item.setText(i)
-            item.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
-            item.setCheckState(QtCore.Qt.Unchecked)
+            self.set_data(i)
 
-        self.listWidget.itemClicked.connect(self.updateLineEdit)
+        if self.lst_data:
+            self.comboBox.setModel(self.listWidget.model())
+            self.comboBox.setView(self.listWidget)
 
-        self.comboBox.setModel(self.listWidget.model())
-        self.comboBox.setView(self.listWidget)
-
-        if self.is_clear:
-            self.lineEdit.clear()
+            if self.is_clear:
+                self.lineEdit.clear()
         return self.comboBox
 
     def updateLineEdit(self, item):
-        print('xx')
-        if item == self.allItem:
-            state = self.allItem.checkState()
-            for i in range(1, self.listWidget.count()):
+        lst_data = []
+        state = item.checkState()
+        if item.text() == '全选':
+            for i in range(self.listWidget.count()):
                 self.listWidget.item(i).setCheckState(state)
+                if state and i != 0:
+                    lst_data.append(self.listWidget.item(i).text())
+            self.lineEdit.setText(", ".join(lst_data))
+        else:
+            curr_text = self.lineEdit.text()
+            if state:
+                self.lineEdit.setText(f'{curr_text},{item.text()}' if curr_text else item.text())
+            else:
+                self.lineEdit.setText(curr_text.replace(item.text(), ''))
 
-        checkedItems = []
-        for i in range(1, self.listWidget.count()):
-            if self.listWidget.item(i).checkState() == QtCore.Qt.Checked:
-                checkedItems.append(self.listWidget.item(i).text())
-        self.lineEdit.setText(", ".join(checkedItems))
